790d22dbe055ae04047ee5e66a717fd340237b5b
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-08 13:31:51 +0530

    rgrg

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <section className=" p-3">
       <div className="top-section flex gap-2">
         <img className="h-8 w-8" src="/phonepaylogo.png"/>
-        <p className=" w-[290px] font-bold rounded-md py-4 pl-4 pr-9 text-[11px] bg-blue-200	text-blue-900">Please help me understand why you made a payment to a froudster.</p>
+        <p className=" w-[290px] font-bold rounded-md py-4 pl-4 pr-9 text-[9px] bg-blue-200	text-blue-900">Please help me understand why you made a payment to a froudster.</p>
       </div>
     </section>
   
