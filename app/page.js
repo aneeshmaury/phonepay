71b9ef282ddb2dfee449ac4cf700aea0f730c943
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-16 07:35:26 +0530

    dfg

--- app/page.js
+++ app/page.js
@@ -25,7 +25,7 @@ const [inputValue, setInputValue] = useState('');
       <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
       <div className="left-chat-top flex ">
 <div className="invisible">jhdjj</div>
-<div className="bg-violet-50 p-3 rounded-sm">
+<div className="bg-violet-50 py-3 px-5 rounded-md">
   <p className="text-blue-700 text-[12px]	font-semibold">I am sorry to know that, ******5472</p>
 </div>
       </div>
